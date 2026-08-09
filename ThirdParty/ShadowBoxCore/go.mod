04836e8c5b4bfa47038f0085830c0a85b98b9663
czczydjl <czczydjl@users.noreply.github.com>
2026-08-09 18:09:10 +0800

    Fix gomobile binding packages and capture Xcode logs

--- ThirdParty/ShadowBoxCore/go.mod
+++ ThirdParty/ShadowBoxCore/go.mod
@@ -4,4 +4,4 @@ go 1.24.7
 
 require github.com/sagernet/sing-box v1.13.16
 
-require github.com/sagernet/gomobile v0.1.12
+require github.com/sagernet/gomobile v0.1.13
--- ThirdParty/ShadowBoxCore/tools.go
+++ ThirdParty/ShadowBoxCore/tools.go
@@ -3,3 +3,5 @@
 package shadowboxcore
 
 import _ "github.com/sagernet/gomobile"
+import _ "github.com/sagernet/gomobile/bind"
+import _ "github.com/sagernet/gomobile/bind/objc"
